9407af81575bcc4cf005b92126306a98ad7e6948
jeronimo-espiga <jeronimo.espiga@correo.ucu.edu.uy>
2020-10-24 22:09:33 -0300

    Agrego precondiciones y postcondiciones

--- Library/Equipment.cs
+++ Library/Equipment.cs
@@ -14,8 +14,56 @@ namespace Full_GRASP_And_SOLID
             this.HourlyCost = hourlyCost;
         }
 
-        public string Description { get; set; }
+        public string Description {
+            get
+            {
+                return this.Description;
+            }
+            set
+            {   // Precondition {P}
+                if (value==null)
+                {
+                    throw new NullValueException("La descripción no puede ser nula");
+                    
+                }   // Precondition {P}
+                else if(value=="")
+                {
+                    throw new EmptyValueException("La descripción no puede estar vacía");
+                }
+                // Operation A
+                this.Description=value;
+                // Postcondition {Q}
+                if (this.Description==null)
+                {
+                    throw new NullValueException("La descripcion no puede ser nula");
+                }
+                // Postcondition {Q}
+                if (this.Description=="")
+                {
+                    throw new EmptyValueException("La descripcion no puede estar vacía");
+                }
+            }
+        } 
 
-        public double HourlyCost { get; set; }
+        public double HourlyCost {
+            get
+            {
+                return this.HourlyCost;
+            } 
+            set
+            {   // Precondition {P}
+                if (value<=0)
+                {   
+                    throw new LessThanZeroException("El costo de hora tiene que ser mayor a cero");
+                }
+                // Operation A
+                this.HourlyCost = value;
+                // Postcondition {Q}
+                if(this.HourlyCost<=0)
+                {
+                    throw new LessThanZeroException("El costo de hora tiene que ser mayor a cero");
+                }
+            }
+        }
     }
 }
--- Library/Product.cs
+++ Library/Product.cs
@@ -14,8 +14,58 @@ namespace Full_GRASP_And_SOLID
             this.UnitCost = unitCost;
         }
 
-        public string Description { get; set; }
+        public string Description {
+               get
+            {
+                return this.Description;
+            }
+            set
+            {   // Precondition {P}
+                if (value==null)
+                {
+                    throw new NullValueException("La descripción no puede ser nula");
+                    
+                }
+                // Precondition {P}
+                else if(value=="")
+                {
+                    throw new EmptyValueException("La descripción no puede estar vacía");
+                }
+                // Operation A
+                this.Description=value;
+                // Postcondition {Q}
+                if (this.Description==null)
+                {
+                    throw new NullValueException("La descripcion no puede ser nula");
+                }
+                // Postcondition {Q}
+                if (this.Description=="")
+                {
+                    throw new EmptyValueException("La descripcion no puede estar vacía");
+                }
+            }
+        } 
 
-        public double UnitCost { get; set; }
+        public double UnitCost {
+            get
+            {
+                return this.UnitCost;
+            } 
+            set
+            {
+                // Precondition {P}
+                if (value<=0)
+                {
+                    throw new LessThanZeroException("El costo de unidad tiene que ser mayor a cero");
+                }
+                // Operation A
+                this.UnitCost = value;
+                 // Postcondition {Q}
+                if(this.UnitCost<=0)
+                {
+                    throw new LessThanZeroException("El costo de la unidad tiene que ser mayor a cero");
+                }
+            }
+        }
     }
 }
